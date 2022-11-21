de713c08a485eac52635cf2a07f3d8e7dd00d0f1
hoanghai1212 <hoanghai129612@gmail.com>
2022-11-21 21:54:40 +0700

    feat: add basic logo component

---
+++ components/atoms/Logo.tsx
@@ -0,0 +1,8 @@
+export function Logo() {
+  return (
+    <div class="flex flex-row items-center justify-center space-x-4">
+      <img src="/logo.svg" class="w-16 h-16" alt="the fresh logo: a sliced lemon dripping with juice" />
+      <div class="text-gray-200 text-2xl font-semibold">Fresh</div>
+    </div>
+  );
+}
